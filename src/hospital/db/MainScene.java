64a8d450f0a079a04c1d73e5f349800375d55551
theorist <theorist17@gmail.com>
2019-05-21 22:35:20 +0900

    fix interface for mysql procedure

--- src/hospital/db/MainScene.java
+++ src/hospital/db/MainScene.java
@@ -244,7 +244,7 @@ public class MainScene {
 			Doctor doctor = dbManager.getDoctor(patientID);
 			if(doctor != null) {
 				//String name, int doctorID, String timeStart, String timeEnd, String department, int onDept
-				if(!dbManager.executeCheckDoctorAppoint(patientID, doctorID, timeStart, timeEnd, dbManager.getDoctor(doctorID).getDepartment(), 1)) {
+				if(!dbManager.executeCheckDoctorAppoint(patientID, doctorID, timeStart, timeEnd, dbManager.getDoctor(doctorID).getDepartment(), 0)) {
 					System.out.println(doctorID + "번 의사에게 예약 할 수 없음");
 					// 5.1 이미 어떤 다른 환자가 자신을 지정해서 진료를 예약한 시간이거나, 진료과만 지정해서 예약했지만 그때 자신의 진료과에 자신 밖에 없는 시간이면, 그 시간 구간에는 자신은 진료받지 못합니. (일 우선)     
 					// 5.2 이미 어떤 다른 환자가 진료과만 지정해서 예약했고 자신이 담당하기로 되었더라도, 그 시간에 다른 (같은 진료과의) 의사가 있고 스케쥴이 비어있으면 자신은 진료예약 할 수 있습니다.
